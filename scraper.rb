298dfc133671c49cd445f7c15c215f14c89fb4ad
Tony Bowden <tony@tmtm.com>
2017-09-28 07:18:04 +0100

    adjust for changed layout

--- scraper.rb
+++ scraper.rb
@@ -3,19 +3,19 @@
 
 require 'wikidata/fetcher'
 
-en_9 = EveryPolitician::Wikidata.wikipedia_xpath( 
+en_9 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_ninth_Parliament_of_Uganda',
-  after: '//span[@id="List_of_members"]',
-  before: '//span[@id="References"]',
+  after: '//span[@id="Elected_members"]',
+  before: '//span[@id="Ex_officio_members"]',
   xpath: '//table//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
-en_10 = EveryPolitician::Wikidata.wikipedia_xpath( 
+en_10 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_tenth_Parliament_of_Uganda',
   after: '//span[@id="List_of_members"]',
   before: '//span[@id="References"]',
   xpath: '//table//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_9 | en_10 })
 
